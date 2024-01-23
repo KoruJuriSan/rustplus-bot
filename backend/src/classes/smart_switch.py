dd22fa897fc751bd762cfd37bf7f48e8d5737aa5
KoruJuriSan <mr.collejoulian@gmail.com>
2024-01-23 18:33:35 +0100

    add: smart_switch classe

---
+++ backend/src/classes/smart_switch.py
@@ -0,0 +1,20 @@
+class Smart_switch():
+    entity_id: int
+    is_on: bool
+    name: str
+    desc: str
+
+    def __init__(self, entity_id: int, name: str, desc: str):
+        self.entity_id = entity_id
+        self.name = name
+        self.desc = desc
+        self.is_on = False
+
+
+    async def toggle(self, socket):
+        if (self.is_on):
+            await socket.turn_off_smart_switch(self.entity_id)
+            self.is_on = False
+        else:
+            await socket.turn_on_smart_switch(self.entity_id)
+            self.is_on = True
