9262f857e1223b5ded7831bc7891380be088fe02
KoruJuriSan <mr.collejoulian@gmail.com>
2024-01-24 14:20:09 +0100

    add: docstring in

--- backend/src/scripts/check_if_dict_elements_are_defined.py
+++ backend/src/scripts/check_if_dict_elements_are_defined.py
@@ -1,4 +1,14 @@
 def check_if_dict_elements_are_defined(dictionnary):
+    """Check for every value in the dictionnary, and tell if one of the value is == to None
+
+    Args:
+        dictionnary (dict): the dictionnary to check
+    
+    Returns:
+        [bool, dictionnary_value || None]
+        bool: if there is any Undefined (None) value in a dictionnary"
+        dictionnary_value: if there is an Undefined (None) value, the key of this item in the dictionnary
+    """
     i = 0
     dictionnary_items = [*dictionnary.items()]
     while i < len(dictionnary_items)-1 and dictionnary_items[i][1] is not None:
