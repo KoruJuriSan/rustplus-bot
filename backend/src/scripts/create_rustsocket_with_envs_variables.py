5316e96541d33c54c2190606fb82fc8c1565fe85
KoruJuriSan <mr.collejoulian@gmail.com>
2024-01-26 10:54:30 +0100

    add: command support in rustsocket creation

--- backend/src/scripts/create_rustsocket_with_envs_variables.py
+++ backend/src/scripts/create_rustsocket_with_envs_variables.py
@@ -1,17 +1,20 @@
-from rustplus import RustSocket
+from rustplus import RustSocket, CommandOptions
 from dotenv import load_dotenv
 from src.scripts.read_envs_to_dict import read_envs_to_dict
 from src.scripts.check_if_dict_elements_are_defined import check_if_dict_elements_are_defined
 
+cmd_prefix = "#"
+
 def create_rust_socket_with_envs_variables():
-    socket = None
+    socket: RustSocket or None = None
     load_dotenv()
-    envs = ["IP", "PORT", "STEAMID", "TOKEN"]
-    info = read_envs_to_dict(envs)
+    envs: list = ["IP", "PORT", "STEAMID", "TOKEN"]
+    info: dict = read_envs_to_dict(envs)
+    cmd_options = CommandOptions(cmd_prefix)
     [is_definied, element_not_defined] = check_if_dict_elements_are_defined(info)
     
     if (not is_definied):
         print(f"La variable d'environnement {element_not_defined} n'existe pas ou n'a pas de valeur !")
     else:
-        socket = RustSocket(info["IP"], info["PORT"], int(info["STEAMID"]), int(info["TOKEN"]))
+        socket = RustSocket(info["IP"], info["PORT"], int(info["STEAMID"]), int(info["TOKEN"]), command_options=cmd_options)
     return socket
