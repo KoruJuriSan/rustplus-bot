2b2f297a616730d7f13fdaf9defaa1c1e599aefe
KoruJuriSan <mr.collejoulian@gmail.com>
2024-01-26 10:29:44 +0100

    add: percision on class type of the socket in smart_switch classe

--- backend/src/classes/smart_switch.py
+++ backend/src/classes/smart_switch.py
@@ -1,8 +1,11 @@
+from  rustplus import RustSocket
+
 class Smart_switch():
     entity_id: int
     is_on: bool
     name: str
     desc: str
+
 
     def __init__(self, entity_id: int, name: str, desc: str):
         self.entity_id = entity_id
@@ -11,7 +14,7 @@ class Smart_switch():
         self.is_on = False
 
 
-    async def toggle(self, socket):
+    async def toggle(self, socket: RustSocket):
         if (self.is_on):
             await socket.turn_off_smart_switch(self.entity_id)
             self.is_on = False
